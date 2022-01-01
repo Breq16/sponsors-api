3dcffd80c07a0b6e080b3ac80456dae89f692278
Brooke Chalmers <breq@breq.dev>
2021-12-31 23:45:07 -0500

    Fetch sponsors from GraphQL

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+import { Octokit } from 'octokit'
+
+const github = new Octokit({
+  auth: GITHUB_TOKEN,
+})
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -6,7 +12,27 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const data = await github.graphql(`
+    query {
+      user(login: "breq16") {
+        sponsors(first: 100) {
+          edges {
+            node {
+              ... on User { login }
+              ... on Organization { login }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return new Response(
+    JSON.stringify({
+      sponsors: data.user.sponsors.edges.map(({ node }) => node.login),
+    }),
+    {
+      headers: { 'content-type': 'application/json' },
+    },
+  )
 }
